b216f4f6790148cce1d38846c3adab9af66218f8
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-03-06 20:29:19 -0800

    initial working notice block

---
+++ plugin.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Plugin Name: Custom Gutenberg Blocks
+* Author: Lorelei Miyamura
+* Description: A library of all custom gutenberg blocks
+* Version: 1.0
+*/
+
+// Loads things when the admin editor is active
+function admin() {
+
+   //Testing
+   wp_enqueue_script('template', plugins_url('/blocks/template/index.js', __FILE__), array( 'wp-blocks', 'wp-element' ));
+   wp_enqueue_style('template', plugins_url( '/blocks/template/index.css', __FILE__ )); //this shows the style in the editor
+}
+add_action( 'enqueue_block_editor_assets', 'admin' );
+
+
+//without this function, the styles won't load on the frontend
+function frontend() {
+   wp_enqueue_style('template', plugins_url( '/template/index.css', __FILE__ ));
+}
+add_action( 'wp_enqueue_scripts', 'frontend' );
+?>
+
