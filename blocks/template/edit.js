14ec4cffa2cd4fe6937d55f632940d0f12fb58fd
Lorelei Miyamura <lorelei.miyamura@gmail.com>
2021-06-02 11:03:43 -0700

    edit.js as a functional const

--- blocks/template/edit.js
+++ blocks/template/edit.js
@@ -1,47 +1,49 @@
-export const edit = (props) => {
-  // How our block renders in the editor in edit mode
- 
-  function updateTitle( event ) {
-     props.setAttributes( { title: event.target.value } );
-  }
-  function updateContent( newdata ) {
-     props.setAttributes( { content: newdata } );
-  }
-  function updateType( event ) {
-     props.setAttributes( { type: event.target.value } );
-  }
-  return el( 'div', 
-     { 
-        className: 'notice-box notice-' + props.attributes.type
-     }, 
-     el(
-        'select', 
-        {
-           onChange: updateType,
-           value: props.attributes.type,
-        },
-        el("option", {value: "default" }, "Default"),
-        el("option", {value: "success" }, "Success"),
-        el("option", {value: "danger" }, "Danger Zone")
-     ),
-     el(
-        'input', 
-        {
-           type: 'text', 
-           placeholder: 'Enter title here...',
-           value: props.attributes.title,
-           onChange: updateTitle,
-           style: { width: '100%' }
-        }
-     ),
-     el(
-        wp.editor.RichText,
-        {
-           tagName: 'p',
-           onChange: updateContent,
-           value: props.attributes.content,
-           placeholder: 'Enter description here...'
-        }
-     )   
-  ); // End return
+var el = wp.element.createElement;
+
+export default function edit({attributes, setAttributes}) {
+   // How our block renders in the editor in edit mode
+  
+   function updateTitle( event ) {
+      setAttributes( { title: event.target.value } );
+   }
+   function updateContent( newdata ) {
+      setAttributes( { content: newdata } );
+   }
+   function updateType( event ) {
+      setAttributes( { type: event.target.value } );
+   }
+   return el( 'div', 
+      { 
+         className: 'notice-box notice-' + attributes.type
+      }, 
+      el(
+         'select', 
+         {
+            onChange: updateType,
+            value: attributes.type,
+         },
+         el("option", {value: "default" }, "Default"),
+         el("option", {value: "success" }, "Success"),
+         el("option", {value: "danger" }, "Danger Zone")
+      ),
+      el(
+         'input', 
+         {
+            type: 'text', 
+            placeholder: 'Enter title here...',
+            value: attributes.title,
+            onChange: updateTitle,
+            style: { width: '100%' }
+         }
+      ),
+      el(
+         wp.editor.RichText,
+         {
+            tagName: 'p',
+            onChange: updateContent,
+            value: attributes.content,
+            placeholder: 'Enter description here...'
+         }
+      )   
+   ); // End return
 }; // End edit()
